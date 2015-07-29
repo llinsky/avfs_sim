334809f6a7261309c67422214a32c9491fe3eac0
Timothy F <timferrell83@yahoo.com>
2015-07-28 21:41:11 -0700

    Started some research. Code still pretty blank

---
+++ get_lut_value.m
@@ -0,0 +1,11 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Function containing logic to output the next frequency and voltage
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function val = get_lut_value(temp,f_curr,counter)
+
+%What should go here? Research in progress ;)
+
+
+end
