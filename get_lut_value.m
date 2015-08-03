17c11dd799341c77f7fae2c48a924d1652343d6d
Leo Linsky <leolinsky@gmail.com>
2015-08-03 18:56:06 +0000

    get_lut_value.m edited online with Bitbucket

--- get_lut_value.m
+++ get_lut_value.m
@@ -16,19 +16,34 @@ function  [T_next, v_next] = get_lut_value(temp_curr,T_curr,v_curr,counter,T_map
 	
 	
 	%leo tentative template
-	v_min = f(T_curr);
-	countersize = 15; (max value)
+	v_min = function1(temp_curr); %this is somewhat arbitrary, just use a somewhat realistic value for an existing CMOS platform.
+	%Also note that v_min will need to be recalibrated for frequency.. TODO
+	countersize = 15; %(max value)
 	estimated_current_workload = (counter/countersize);
 	
-	%For now let's say that we want a 90% workload. Find optimal power given allowable voltages and frequencies to maintain 90% workload or lower
+	T_max = estimated_current_workload/T_curr; %T_max = f_min to maintain a 100 percent workload (and not fall behind) 
+	
+	%Now find optimal power given allowable voltages and frequencies to maintain 90% workload or lower
+	infinity = 9999999;
+	bestPower = infinity;
+	
+	power = 0;
 	
 	for i=1:(size(v_map))
 		if (v_map(i) < v_min)
 			continue;
 		end
 		for j=1:(size(T_map))
+			if (T_map(j) > T_max)
+				continue;
+			end
 			
-			
+			power = function2(T_map(j),v_map(i));
+			if (power < bestPower)
+				bestPower = power;
+				T_next = T_map(j);
+				v_next = v_map(i);
+			end
 		end
 	end
 
