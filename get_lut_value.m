3ce180dd5a35e3b0cdea274308a4245b2348e8d0
Timothy F <timferrell83@yahoo.com>
2015-08-02 22:34:54 -0700

    more equations I found

--- get_lut_value.m
+++ get_lut_value.m
@@ -7,9 +7,11 @@ function  [T_next, v_next] = get_lut_value(temp_curr,T_curr,v_curr,counter,T_map
 	P = CV^2*A*f
 	-how does freq depend on voltage?
 	f = (Vdd - Vth)^(alpha)/k*Vdd, k is constant, 1<alpha<2
-	-how does temp depend on voltage?
+	-how does threshold voltage depend on temperature?
+	threshold voltage decreases as temperature increases...
+	I would maybe just define Vth=(-a)T + b, a and b constants, T<some upper bound
 	%select correct T value 
-
+	
 	%select correct V value 
 
 end
