77d2cad5e1b884fd86ea5b2b5357dee86d13def1
Timothy F <timferrell83@yahoo.com>
2015-08-02 22:13:10 -0700

    New equations in get_lut_value

--- get_lut_value.m
+++ get_lut_value.m
@@ -6,8 +6,8 @@
 function  [T_next, v_next] = get_lut_value(temp_curr,T_curr,v_curr,counter,T_map,v_map)
 	P = CV^2*A*f
 	-how does freq depend on voltage?
+	f = (Vdd - Vth)^(alpha)/k*Vdd, k is constant, 1<alpha<2
 	-how does temp depend on voltage?
-	
 	%select correct T value 
 
 	%select correct V value 
