e4287ba69998e2501db1f633a97ed45163b2392b
Leo Linsky <LEO@leo-linskys-macbook-2.local>
2015-08-03 21:36:06 -0700

    Please fix my problems

--- get_lut_value.m
+++ get_lut_value.m
@@ -4,12 +4,12 @@
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function  [T_next, v_next] = get_lut_value(temp_curr,T_curr,v_curr,counter,T_map,v_map)
-	P = CV^2*A*f
-	-how does freq depend on voltage?
-	f = (Vdd - Vth)^(alpha)/k*Vdd, k is constant, 1<alpha<2
-	-how does threshold voltage depend on temperature?
-	threshold voltage decreases as temperature increases...
-	I would maybe just define Vth=(-a)T + b, a and b constants, T<some upper bound
+% 	P = CV^2*A*f
+% 	-how does freq depend on voltage?
+% 	f = (Vdd - Vth)^(alpha)/k*Vdd, k is constant, 1<alpha<2
+% 	-how does threshold voltage depend on temperature?
+% 	threshold voltage decreases as temperature increases...
+% 	I would maybe just define Vth=(-a)T + b, a and b constants, T<some upper bound
 	%select correct T value 
 	
 	%select correct V value 
